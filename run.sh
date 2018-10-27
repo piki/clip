eebaae26bf5180bbcbfbbfdd9072169f10e12906
Patrick Reynolds <patrick.reynolds@github.com>
2018-10-27 00:34:41 -0400

    it speaks

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo ' ______________________________________'
+echo "/ It looks like you're trying to build \\"
+printf "\ %-36s /\n" $GITHUB_REPOSITORY
+echo ' --------------------------------------'
+echo ' \'
+echo '  \'
+echo '     __'
+echo '    /  \'
+echo '    |  |'
+echo '    @  @'
+echo '    |  |'
+echo '    || |/'
+echo '    || ||'
+echo '    |\_/|'
+echo '    \___/'
